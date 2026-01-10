be87234111c2ffabc1a36a894e2be928b54c2bff
Roshan Maurya <roshan.maurya90@gmail.com>
2026-01-10 23:54:33 +0530

    changes

--- app/services/ai_service.py
+++ app/services/ai_service.py
@@ -1,9 +1,10 @@
 import requests
+import os
+from dotenv import load_dotenv
 
-OLLAMA_URL = "http://localhost:11434/api/generate"
+load_dotenv()
 
 def generate_summary(text):
-    print(text)
     system_prompt = (
         "You are an expert book summarization assistant. "
         "Generate a concise, neutral, and factual summary in 4–5 sentences. "
@@ -12,28 +13,14 @@ def generate_summary(text):
         "respond with 'Cannot generate a summary: content too short.'"
     )
 
-    # print(len(text.splitlines()),'kkkkkkkkkkk')
-
     if not text or len(text.replace(' ','')) <= 50:
         return "Cannot generate a summary: content too short. Please provide at least 50 words."
-    
-
-    # payload = {
-    #     "model": "llama3.2",
-    #     "messages": [
-    #         {"role": "system", "content": system_prompt},
-    #         {"role": "user", "content": f"Summarize the following book content:\n{text}",}
-    #     ],
-    #     "stream": False
-    # }
 
     
     payload = {
-        "model": "llama3.2",
-        # "system_prompt":system_prompt,
+        "model": os.getenv('model'),
         "prompt": f"{system_prompt}\n\n{text}",
         "stream": False
     }
-    response = requests.post(OLLAMA_URL, json=payload)
-    print(response,'response========')
+    response = requests.post(os.getenv('OLLAMA_URL'), json=payload)
     return response.json().get("response", "")
